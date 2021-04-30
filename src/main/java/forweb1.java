38f4ebb298f8eaf3f8a0f690aaba6408dcca6293
sachin sharma <schnshrma01@gmail.com>
2021-04-30 10:17:14 +0530

    updated

--- src/main/java/forweb1.java
+++ src/main/java/forweb1.java
@@ -1,4 +1,3 @@
-import com.github.jsonldjava.shaded.com.google.common.collect.HashBasedTable;
 import org.apache.jena.ontology.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.*;
@@ -8,7 +7,7 @@ import org.neo4j.driver.Driver;
 import java.io.*;
 import java.util.*;
 
-public class infrencing {
+public class forweb1 {
     public static void main(String[] args) {
         Driver driver = GraphDatabase.driver(
                 "bolt://localhost:7687", AuthTokens.basic("neo4j", Pass.pass));
@@ -31,7 +30,7 @@ public class infrencing {
         ExtendedIterator<OntClass> iterator = model.listClasses();
         List<String> classes = new ArrayList<String>();
         HashMap<String, List<String>> ClassSuperclassUris = new HashMap<String, List<String>>();
-       // HashMap<String, String> UrisToClassLabel = new HashMap<String, String>();
+        // HashMap<String, String> UrisToClassLabel = new HashMap<String, String>();
         List<List<String>> AllProps = new ArrayList<List<String>>();
         while (iterator.hasNext()) {
             OntClass ontClass = (OntClass) iterator.next();
@@ -135,6 +134,9 @@ public class infrencing {
         //hasmaps
         HashMap<String, Set<String>> stp = new HashMap<String, Set<String>>();
         HashMap<List<String>, Set<String>> pto = new HashMap<List<String>, Set<String>>();
+        HashMap<String, Set<String>> stpp = new HashMap<String, Set<String>>();
+        HashMap<List<String>, Set<String>> ptoo = new HashMap<List<String>, Set<String>>();
+
         Iterator indi = model.listIndividuals();
         while (indi.hasNext()) {
             Individual indiv = (Individual) indi.next();
@@ -142,35 +144,35 @@ public class infrencing {
         }
         StmtIterator iterator1 = model.listStatements();
         int d = 0;
-        HashMap<String,String> inverse=new HashMap<String, String>();
-        ExtendedIterator<ObjectProperty> nodeIterator = model.listObjectProperties();
-        while (nodeIterator.hasNext()) {
-            ObjectProperty objectProperty = (ObjectProperty) nodeIterator.next();
-            ExtendedIterator<? extends OntProperty> extendedIterator = objectProperty.listInverseOf();
-            if (extendedIterator.hasNext()) {
-                System.out.println("Object property is : " + objectProperty.getLocalName());
-                System.out.println("Inverse Object property of: " + objectProperty.getInverseOf().getLocalName());
-                inverse.put(objectProperty.getLocalName(),objectProperty.getInverseOf().getLocalName());
-               //inverse.put(objectProperty.getInverseOf().getLocalName(),objectProperty.getLocalName());
-            }
-        }
-            while (iterator1.hasNext()) {
-                System.out.println("*************************************");
-                Statement statement = iterator1.nextStatement();
-                Resource subject = statement.getSubject();
-                Property predicate = statement.getPredicate();
-                RDFNode object = statement.getObject();
-                String sub, pred, obj;
-                if (predicate.getLocalName().equals("label") || predicate.getLocalName().equals("rest") || predicate.getLocalName().equals("first") || predicate.getLocalName().equals("members")) {
-                    continue;
-                } else if (predicate.getLocalName().equals("ingredients") || predicate.getLocalName().equals("description") || predicate.getLocalName().equals("cusineName") || predicate.getLocalName().equals("startDate") || predicate.getLocalName().equals("endDate") || predicate.getLocalName().equals("cusineName") || predicate.getLocalName().equals("address") || predicate.getLocalName().equals("name")) {
+//        HashMap<String,String> inverse=new HashMap<String, String>();
+//        ExtendedIterator<ObjectProperty> nodeIterator = model.listObjectProperties();
+//        while (nodeIterator.hasNext()) {
+//            ObjectProperty objectProperty = (ObjectProperty) nodeIterator.next();
+//            ExtendedIterator<? extends OntProperty> extendedIterator = objectProperty.listInverseOf();
+//            if (extendedIterator.hasNext()) {
+//                System.out.println("Object property is : " + objectProperty.getLocalName());
+//                System.out.println("Inverse Object property of: " + objectProperty.getInverseOf().getLocalName());
+//                inverse.put(objectProperty.getLocalName(),objectProperty.getInverseOf().getLocalName());
+//                inverse.put(objectProperty.getInverseOf().getLocalName(),objectProperty.getLocalName());
+//            }
+//        }
+        while (iterator1.hasNext()) {
+            System.out.println("*************");
+            Statement statement = iterator1.nextStatement();
+            Resource subject = statement.getSubject();
+            Property predicate = statement.getPredicate();
+            RDFNode object = statement.getObject();
+            String sub, pred, obj;
+            if (predicate.getLocalName().equals("label") || predicate.getLocalName().equals("rest") || predicate.getLocalName().equals("first") || predicate.getLocalName().equals("members")) {
+                continue;
+            } else if (predicate.getLocalName().equals("ingredients") || predicate.getLocalName().equals("description") || predicate.getLocalName().equals("cusineName") || predicate.getLocalName().equals("startDate") || predicate.getLocalName().equals("endDate") || predicate.getLocalName().equals("cusineName") || predicate.getLocalName().equals("address") || predicate.getLocalName().equals("name")) {
 
-                    System.out.println("Subject is: " + subject.getLocalName());
-                    System.out.println("Predicate is: " + predicate.getLocalName());
-                    System.out.println("Object is: " + object.toString());
-                    sub = subject.getLocalName();
-                    pred = predicate.getLocalName();
-                    obj = object.toString();
+                System.out.println("Subject is: " + subject.getLocalName());
+                System.out.println("Predicate is: " + predicate.getLocalName());
+                System.out.println("Object is: " + object.toString());
+                sub = subject.getLocalName();
+                pred = predicate.getLocalName();
+                obj = object.toString();
 //                if (!stp.containsKey(subject.getLocalName())) {
 //                    stp.put(subject.getLocalName(), new ArrayList<String>());
 //                }
@@ -182,13 +184,13 @@ public class infrencing {
 //                    pto.put( list , new ArrayList<String>());
 //                }
 //                pto.get(list).add(object.toString());
-                } else if (predicate.getLocalName().equals("price") || predicate.getLocalName().equals("city")) {
-                    System.out.println("Subject is: " + subject.getLocalName());
-                    System.out.println("Predicate is: " + predicate.getLocalName());
-                    System.out.println("Object is: " + object.toString().replaceAll("\\^\\^http://www.w3.org/2001/XMLSchema#int", ""));
-                    sub = subject.getLocalName();
-                    pred = predicate.getLocalName();
-                    obj = object.toString().replaceAll("\\^\\^http://www.w3.org/2001/XMLSchema#int", "");
+            } else if (predicate.getLocalName().equals("price") || predicate.getLocalName().equals("city")) {
+                System.out.println("Subject is: " + subject.getLocalName());
+                System.out.println("Predicate is: " + predicate.getLocalName());
+                System.out.println("Object is: " + object.toString().replaceAll("\\^\\^http://www.w3.org/2001/XMLSchema#int", ""));
+                sub = subject.getLocalName();
+                pred = predicate.getLocalName();
+                obj = object.toString().replaceAll("\\^\\^http://www.w3.org/2001/XMLSchema#int", "");
 
 //                    if (!stp.containsKey(subject.getLocalName())) {
 //                        stp.put(subject.getLocalName(), new ArrayList<String>());
@@ -202,16 +204,16 @@ public class infrencing {
 //                    }
 //                    pto.get(list).add(object.toString().replaceAll("\\^\\^http://www.w3.org/2001/XMLSchema#int", ""));
 //                    //stp.get(predicate.getLocalName()).add(object.toString().replaceAll("\\^\\^http://www.w3.org/2001/XMLSchema#int", ""));
-                } else {
-                    System.out.println("Subject is: " + subject.getLocalName());
-                    System.out.println("Predicate is: " + predicate.getLocalName());
-                    String uri = object.toString();
-                    String[] arrOfStr = uri.split("#", 2);
-                    System.out.println("Object is: " + arrOfStr[1]);
-                    //System.out.println("isko split krna pdhaega");
-                    sub = subject.getLocalName();
-                    pred = predicate.getLocalName();
-                    obj = arrOfStr[1];
+            } else {
+                System.out.println("Subject is: " + subject.getLocalName());
+                System.out.println("Predicate is: " + predicate.getLocalName());
+                String uri = object.toString();
+                String[] arrOfStr = uri.split("#", 2);
+                System.out.println("Object is: " + arrOfStr[1]);
+                //System.out.println("isko split krna pdhaega");
+                sub = subject.getLocalName();
+                pred = predicate.getLocalName();
+                obj = arrOfStr[1];
 //                    if (!stp.containsKey(subject.getLocalName())) {
 //                        stp.put(subject.getLocalName(), new ArrayList<String>());
 //                    }
@@ -226,62 +228,67 @@ public class infrencing {
 //                    // stp.get(predicate.getLocalName()).add(arrOfStr[1]);
 //
 //                }
-                }
-                if (!stp.containsKey(sub)) {
-                    stp.put(sub, new HashSet<String>());
-                }
-                stp.get(sub).add(pred);
-                List<String> list = new ArrayList<String>();
-                list.add(sub);
-                list.add(pred);
-                if (!pto.containsKey(list)) {
-                    pto.put(list, new HashSet<String>());
-                }
-                pto.get(list).add(obj);
-
-                int flag = 1;
-                while (flag == 1) {
-
-                    if (inverse.containsKey(pred)) {
-                        if (!stp.containsKey(obj)) {
-                            stp.put(obj, new HashSet<String>());
-                        }
-                        stp.get(obj).add(inverse.get(pred));
-                        List<String> list1 = new ArrayList<String>();
-                        list1.add(obj);
-                        list1.add(inverse.get(pred));
-                        if (!pto.containsKey(list1)) {
-                            pto.put(list1, new HashSet<String>());
-                        }
-                        int s = pto.get(list1).size();
-                        pto.get(list1).add(sub);
-                        if(s==pto.get(list1).size())
-                        { flag=0; break;}
-
-                    }
-                    else {
-                        flag=0;
-                    }
-                    pred=inverse.get(pred);
-                    String tempo=sub;
-                    sub=obj;
-                    obj=tempo;
-                }
-
             }
+            if (!stp.containsKey(sub)) {
+                stp.put(sub,new HashSet<String>());
+            }
+            stp.get(sub).add(pred);
+            List<String> list = new ArrayList<String>();
+            list.add(sub);
+            list.add(pred);
+            if (!pto.containsKey(list)) {
+                pto.put(list, new HashSet<String>());
+            }
+            pto.get(list).add(obj);
 
-            int a = 1;
+            if (!stpp.containsKey(obj)) {
+                stpp.put(obj,new HashSet<String>());
+            }
+            stpp.get(obj).add(pred);
+            List<String> list11 = new ArrayList<String>();
+            list11.add(obj);
+            list11.add(pred);
+            if (!ptoo.containsKey(list11)) {
+                ptoo.put(list11, new HashSet<String>());
+            }
+            ptoo.get(list11).add(sub);
+
+
+//            if(inverse.containsKey(pred))
+//            {
+//                if (!stp.containsKey(obj)) {
+//                    stp.put(obj,new HashSet<String>());
+//                }
+//                stp.get(obj).add(inverse.get(pred));
+//                List<String> list1 = new ArrayList<String>();
+//                list1.add(obj);
+//                list1.add(inverse.get(pred));
+//                if (!pto.containsKey(list1)) {
+//                    pto.put(list1, new HashSet<String>());
+//                }
+//                pto.get(list1).add(sub);
+//            }
+        }
+
+
+        System.out.println("\nPress 1 to select subject or " +
+                "\nPress 2 to select object");
+        Scanner scan = new Scanner(System.in);
+        int a = Integer.parseInt(scan.nextLine());
+
+        while (a == 1 || a == 2) {
             while (a == 1) {
-                System.out.println("*************************************#####################*************************************");
+                System.out.println("************#####################************");
                 System.out.println("select subject from following list:");
                 for (Map.Entry mapElement : stp.entrySet()) {
                     String key = (String) mapElement.getKey();
                     System.out.println(key);
 
                 }
-                Scanner scan = new Scanner(System.in);
+
                 System.out.print("Enter your subject: ");
-                String name_s = scan.nextLine();
+                Scanner scan1 = new Scanner(System.in);
+                String name_s = scan1.nextLine();
 
                 Set<String> list = new HashSet<String>();
 
@@ -289,7 +296,8 @@ public class infrencing {
                 System.out.println("select predicate from following list");
                 System.out.println(list);
                 System.out.print("Enter your predicate: ");
-                String name_p = scan.nextLine();
+                Scanner scan2 = new Scanner(System.in);
+                String name_p = scan2.nextLine();
                 Set<String> list_o = new HashSet<String>();
 
                 List<String> temp = new ArrayList<String>();
@@ -303,9 +311,49 @@ public class infrencing {
                 System.out.println("With subject '" + name_s + "' predicate '" + name_p + "' object is => " + list_o + "\n\n");
                 System.out.println(name_s + " " + name_p + " " + list_o + "\n\n");
 
-                System.out.println(" To Continue press 1 ");
+                System.out.println("Press 1 to select subject or \nPress 2 to select object");
                 a = scan.nextInt();
             }
 
+
+            while (a == 2) {
+                System.out.println("************#####################************");
+                System.out.println("select object from following list:");
+                for (Map.Entry mapElement : stpp.entrySet()) {
+                    String key = (String) mapElement.getKey();
+                    System.out.println(key);
+
+                }
+
+                System.out.print("Enter your object: ");
+                Scanner scan3 = new Scanner(System.in);
+                String name_s1 = scan3.nextLine();
+
+                Set<String> list = new HashSet<String>();
+
+                list = stpp.get(name_s1);
+                System.out.println("select predicate from following list");
+                System.out.println(list);
+                System.out.print("Enter your predicate: ");
+                Scanner scan4 = new Scanner(System.in);
+                String name_p = scan4.nextLine();
+                Set<String> list_o = new HashSet<String>();
+
+                List<String> temp = new ArrayList<String>();
+                temp.add(name_s1);
+                temp.add(name_p);
+                list_o = ptoo.get(temp);
+
+                System.out.println(list_o);
+
+                System.out.println("FINAL RESULT\n\n");
+                System.out.println("With object '" + name_s1 + "' predicate '" + name_p + "' subject is => " + list_o + "\n\n");
+                System.out.println( list_o+ " " + name_p + " " + name_s1 + "\n\n");
+
+                System.out.println("Press 1 to select subject or \nPress 2 to select object");
+                a = scan.nextInt();
+            }
         }
     }
+
+}
